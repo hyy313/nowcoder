fda54061f6be9b8b626665e14ce5b70af86a5208
coderh <200938514@qq.com>
2019-07-16 15:35:28 +0800

    剑指offer 面试题9：斐波那契数列 complete 注：递归浪费时间空间，观察斐波那契数列的规律可以用循环解决，而不需要用栈转换递归。

---
+++ nowcoder/9.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int Fibonacci(int n) {
+
+		int a = 0;
+		int b = 1;
+
+		if (n <= 0) {
+			return a;
+		}
+
+		if (n == 1) {
+			return b;
+		}
+
+		for (int i = 2; i <= n; i++) {
+			int temp = b;
+			b += a;
+			a = temp;
+		}
+
+		return b;
+	}
+};
+
+int main() {
+	Solution s;
+	
+	int n;
+
+	cout << "ÊäÈën:";
+	cin >> n;
+
+	while (n != -1) {
+		cout << "Fibonacci(n):" << s.Fibonacci(n) << endl;
+
+		cout << "ÊäÈën:";
+		cin >> n;
+	}
+}
