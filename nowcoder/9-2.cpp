a35cc7278eadcfc60fd8d3f4ca236a43aa6b62cf
coderh <200938514@qq.com>
2019-07-16 16:10:31 +0800

    剑指offer 面试题9-2：斐波那契数列应用：变态跳台阶 complete 注：每次可以跳最多n个台阶，就是数学问题！

---
+++ nowcoder/9-2.cpp
@@ -0,0 +1,28 @@
+//#include<iostream>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	int jumpFloorII(int number) {
+//
+//		return pow(2, number-1);
+//
+//	}
+//
+//};
+//
+//int main() {
+//	Solution s;
+//	
+//	int n;
+//
+//	cout << "ÊäÈën:";
+//	cin >> n;
+//
+//	while (n != -1) {
+//		cout << "jumpFloorII(n):" << s.jumpFloorII(n) << endl;
+//
+//		cout << "ÊäÈën:";
+//		cin >> n;
+//	}
+//}
