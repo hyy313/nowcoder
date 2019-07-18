8bc67861a318803ad5ff183c3eb3b7c280bd24d1
coderh <200938514@qq.com>
2019-07-19 00:44:01 +0800

    剑指offer 面试题14-1：调整数组顺序使奇数位于偶数之前（保持奇偶数原来顺序） 注1：用例要考虑全面（）（1，2，3，4，5，6，7）（2，4，6，1，3，5） 注2：要注意循环结束条件，避免死循环，tag到底何时退出 注3：数组崩溃？vs2019中得先删除再插入保证数组不越界，因为我初始化限定了vector的大小。 注4：数组崩溃？newcoder-oj中段错误:您的程序发生段错误，可能是数组越界，堆栈溢出（比如，递归调用层数太多）等情况引起?

---
+++ nowcoder/14-1.cpp
@@ -0,0 +1,63 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	void reOrderArray(vector<int>& array) {
+//
+//		if (array.size() <= 1)
+//			return;
+//
+//		int tag = 0;
+//
+//		for (int i = 0; i <= array.size(); i++) {
+//
+//			if (array[i] % 2 == 0) {
+//
+//				//用于初始化限定vector大小
+//				/*int temp = array[i];
+//				array.erase(begin(array) + i);
+//				array.push_back(temp);*/
+//
+//				array.push_back(array[i]);
+//				array.erase(begin(array) + i);
+//
+//				tag++;
+//				i--;//nowcoder oj中此句注释掉报错：段错误:您的程序发生段错误，可能是数组越界，堆栈溢出（比如，递归调用层数太多）等情况引起
+//			}
+//
+//			if (i + tag == array.size() - 1)
+//				break;
+//
+//		}
+//		return;
+//
+//	}
+//};
+//
+//int main() {
+//
+//	vector<int> a = { 2,4,6,1,3,5,7 };
+//
+//	vector<int> b;
+//
+//	for (int i = 0; i < 7; i++) {
+//		b.push_back(i+1);
+//	}
+//
+//	Solution s;
+//	s.reOrderArray(a);
+//	s.reOrderArray(b);
+//
+//	for (int i = 0; i < a.size(); i++) {
+//		cout << a[i] << " ";
+//	}
+//	cout << endl;
+//
+//	for (int i = 0; i < b.size(); i++) {
+//		cout << b[i] << " ";
+//	}
+//	cout << endl;
+//
+//}
