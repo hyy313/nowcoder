2bc8da2d6bf2b5e5c6608589c2ab1f810230d148
coderh <200938514@qq.com>
2019-09-03 15:54:34 +0800

    剑指offer 面试题24：二叉搜索树的后序遍历序列 complete 注1：注意对输入是否有效进行判断，防止空输入导致循环溢出 注2：树的问题注意前、中、后序遍历

--- nowcoder/23.cpp
+++ nowcoder/23.cpp
@@ -1,60 +1,60 @@
-#include<iostream>
-#include<vector>
-#include<queue>
-using namespace std;
-
-struct TreeNode {
-	int val;
-	struct TreeNode* left;
-	struct TreeNode* right;
-	TreeNode(int x) :
-		val(x), left(NULL), right(NULL) {
-	}
-};
-
-class Solution {
-public:
-	vector<int> PrintFromTopToBottom(TreeNode* root) {
-		vector<int> res;
-		queue<TreeNode*> temp;
-
-		temp.push(root);
-
-		while (!temp.empty()) {
-			TreeNode* p = temp.front();
-			temp.pop();
-
-			if (p) {
-				res.push_back(p->val);
-				if (p->left)
-					temp.push(p->left);
-				if (p->right)
-					temp.push(p->right);
-			}
-		}
-
-		return res;
-	}
-};
-
-int main() {
-
-	TreeNode* root = NULL;
-	/*TreeNode* root = new TreeNode(1);*/
-	/*root->left = new TreeNode(2);
-	root->right = new TreeNode(3);
-	root->left->right = new TreeNode(4);
-	root->right->left = new TreeNode(5);*/
-
-	Solution s;
-	vector<int> res = s.PrintFromTopToBottom(root);
-
-	for (int i = 0; i < res.size(); i++) {
-		cout << res[i] << " ";
-	}
-
-	cout << endl;
-
-	//system("pause");
-
-}
+//#include<iostream>
+//#include<vector>
+//#include<queue>
+//using namespace std;
+//
+//struct TreeNode {
+//	int val;
+//	struct TreeNode* left;
+//	struct TreeNode* right;
+//	TreeNode(int x) :
+//		val(x), left(NULL), right(NULL) {
+//	}
+//};
+//
+//class Solution {
+//public:
+//	vector<int> PrintFromTopToBottom(TreeNode* root) {
+//		vector<int> res;
+//		queue<TreeNode*> temp;
+//
+//		temp.push(root);
+//
+//		while (!temp.empty()) {
+//			TreeNode* p = temp.front();
+//			temp.pop();
+//
+//			if (p) {
+//				res.push_back(p->val);
+//				if (p->left)
+//					temp.push(p->left);
+//				if (p->right)
+//					temp.push(p->right);
+//			}
+//		}
+//
+//		return res;
+//	}
+//};
+//
+//int main() {
+//
+//	TreeNode* root = NULL;
+//	/*TreeNode* root = new TreeNode(1);*/
+//	/*root->left = new TreeNode(2);
+//	root->right = new TreeNode(3);
+//	root->left->right = new TreeNode(4);
+//	root->right->left = new TreeNode(5);*/
+//
+//	Solution s;
+//	vector<int> res = s.PrintFromTopToBottom(root);
+//
+//	for (int i = 0; i < res.size(); i++) {
+//		cout << res[i] << " ";
+//	}
+//
+//	cout << endl;
+//
+//	//system("pause");
+//
+//}
---
+++ nowcoder/24.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+	bool VerifySquenceOfBST(vector<int> sequence) {
+
+		//用来阻止空树进入，防止空树导致的越界
+		if (sequence.size() == 0)
+			return false;
+
+		int i = 0, r = 0;
+
+		//扫描寻找左右子树的起点
+		for (; i < sequence.size() - 1; i++)
+			if (sequence[i] > sequence[sequence.size() - 1])
+				break;
+		r = i;
+
+		for (; i < sequence.size() - 1; i++)
+			if (sequence[i] < sequence[sequence.size() - 1])
+				return false;
+
+		//左右子树初始化
+		bool left = true, right = true;
+		vector<int> vl(sequence.begin(), sequence.begin() + i);
+		vector<int> vr(sequence.begin() + r, sequence.end() - 1);
+
+		if (r > 0)
+			left = VerifySquenceOfBST(vl);
+
+		if (r < i)
+			right = VerifySquenceOfBST(vr);
+
+		return left && right;
+
+	}
+};
+
+int main() {
+
+	//vector<int> s = { 5,7,6,9,11,10,8 };
+	vector<int> s = { 7,8 };
+
+	Solution ss;
+	cout << ss.VerifySquenceOfBST(s) << endl;
+
+}
